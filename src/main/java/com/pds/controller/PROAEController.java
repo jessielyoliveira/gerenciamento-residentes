2b8a4d390345f3e74fa66d947c840c11249c3ba2
Jessiely Oliveira <jessiely.costa@gmail.com>
2019-03-20 18:27:28 -0300

    Gerenciar Residencia - adicionando, listando e removendo

--- src/main/java/com/pds/controller/PROAEController.java
+++ src/main/java/com/pds/controller/PROAEController.java
@@ -1,13 +1,18 @@
 package com.pds.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pds.model.PROAE;
+import com.pds.model.Residencia;
 import com.pds.service.PROAEService;
+import com.pds.service.ResidenciaService;
 
 @Controller
 @RequestMapping("/proae")
--- src/main/java/com/pds/controller/ResidenciaController.java
+++ src/main/java/com/pds/controller/ResidenciaController.java
@@ -2,10 +2,6 @@ package com.pds.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
-import org.h2.util.New;
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.pds.model.Residencia;
 import com.pds.service.ResidenciaService;
@@ -26,10 +21,13 @@ public class ResidenciaController {
 	@Autowired
 	private ResidenciaService residenciaService;
 	
-	/*
-	 * @GetMapping public String indexResidencia() { return
-	 * "residencia/indexResidencia"; }
-	 */
+	
+	@GetMapping
+	public String indexResidencia(Model model) {
+		List<Residencia> lista = residenciaService.findAll();
+		model.addAttribute("listaResidencias", lista);
+		return "residencia/homeResidencia"; 
+	}
 	
 	// Abre o formulario de cadastro de residencias
 	@GetMapping("/nova")
@@ -42,23 +40,16 @@ public class ResidenciaController {
 	@PostMapping("/nova")
 	public String residenciaSubmit(@ModelAttribute Residencia residencia) {
 		residenciaService.save(residencia);
-		return "redirect:/proae";
+		return "redirect:/residencias";
 	}
 	
-	@GetMapping("/remover")
-	public String listarResidencias(Model model) {
-		List<Residencia> lista = residenciaService.findAll();
-		model.addAttribute("listaResidencias", lista);
-		return "residencia/listarResidencias";
-	}
-	
-	@RequestMapping("/remover/{id}")
+	@GetMapping("/remover/{id}")
 	public String remover(@PathVariable("id") Integer id) {
 		if (id != null) {
 			Residencia residencia = residenciaService.findOne(id).get();
 			residenciaService.delete(residencia);
 		}
-		return "redirect:/residencias/remover";
+		return "redirect:/residencias";
 	}
 	
 	
