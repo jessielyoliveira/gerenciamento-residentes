b832a43eaceb420a6b8c4b232ff1c0552422f18c
Jessiely Oliveira <jessiely.costa@gmail.com>
2019-03-24 14:29:12 -0300

    Edição de residência

--- src/main/java/com/pds/controller/ResidenciaController.java
+++ src/main/java/com/pds/controller/ResidenciaController.java
@@ -7,25 +7,26 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pds.model.Residencia;
 import com.pds.service.ResidenciaService;
 
 @Controller
-@RequestMapping("/residencias")
 public class ResidenciaController {
 	
 	@Autowired
 	private ResidenciaService residenciaService;
 	
 	
-	@GetMapping
+	@RequestMapping("/residencias")
 	public String indexResidencia(Model model) {
 		List<Residencia> lista = residenciaService.findAll();
 		model.addAttribute("listaResidencias", lista);
@@ -33,20 +34,21 @@ public class ResidenciaController {
 	}
 	
 	// Abre o formulario de cadastro de residencias
-	@GetMapping("/nova")
-	public String residenciaForm(Model model) {
+	@RequestMapping("/residencias/nova")
+	public String criar(Model model) {
 		model.addAttribute("residencia", new Residencia());
 		return "residencia/formResidencia";
 	}
 	
 	// Envia as informacoes do formulario para a camada de servico
-	@PostMapping
-	public String residenciaSubmit(@ModelAttribute Residencia residencia) {
+	@RequestMapping(value = "/residencias/salvar", method = RequestMethod.POST)
+	public String salvar(@Valid Residencia residencia) {
+		System.out.println(residencia.getId());
 		residenciaService.save(residencia);
 		return "redirect:/residencias";
 	}
 	
-	@GetMapping("/remover/{id}")
+	@GetMapping("/residencias/remover/{id}")
 	public String remover(@PathVariable("id") Integer id) {
 		if (id != null) {
 			Residencia residencia = residenciaService.findOne(id).get();
@@ -55,8 +57,8 @@ public class ResidenciaController {
 		return "redirect:/residencias";
 	}
 	
-	@GetMapping("/editar/{id}")
-	public String atualizar(Model model, @PathVariable("id") Integer id) {
+	@RequestMapping("/residencias/editar/{id}")
+	public String atualizar(Model model, @PathVariable Integer id) {
 		System.out.println(id);
 		if (id != null) {
 			Residencia residencia = residenciaService.findOne(id).get();
@@ -65,14 +67,5 @@ public class ResidenciaController {
 		}
 		return "residencia/formResidencia";
 	}
-	
-	@PutMapping
-	public String atualizar(@ModelAttribute Residencia residencia) {
-		System.out.println(residencia.getId());
-		residenciaService.save(residencia);
-		return "redirect:/residencias";
-	}
-	
-	
 
 }
